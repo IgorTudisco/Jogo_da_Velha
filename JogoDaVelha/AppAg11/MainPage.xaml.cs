5909d132d83dc0742dfe38ba91c5993fbcc5800b
Igor Tudisco <igortudisco@hotmail.com>
2024-10-18 17:01:12 -0300

    Tic-tac-toe

--- JogoDaVelha/AppAg11/MainPage.xaml.cs
+++ JogoDaVelha/AppAg11/MainPage.xaml.cs
@@ -1,25 +1,269 @@
-﻿namespace AppAg11
-{
-    public partial class MainPage : ContentPage
-    {
-        int count = 0;
+﻿namespace AppAg11;
 
-        public MainPage()
+public partial class MainPage : ContentPage
+{
+    public string vez = "X";
+    public Button[,] board;
+    public int rowBoard;
+    public int colBoard;
+    public int maxSquare;
+    public string vitoria = "";
+    public int jogada;
+
+    public MainPage()
+    {
+        InitializeComponent();
+
+        board = new Button[,]
         {
-            InitializeComponent();
+            { btn10, btn11, btn12 },
+            { btn20, btn21, btn22 },
+            { btn30, btn31, btn32 }
+        };
+
+        rowBoard = board.GetLength(0);
+        colBoard = board.GetLength(1);
+        maxSquare = rowBoard * colBoard;
+    }
+
+    private void Button_Clicked(Object sender, EventArgs e)
+    {
+        Button btn = (Button)sender;
+        btn.IsEnabled = false;
+
+        vez = Jogador(btn);
+
+        Jogadas();
+
+        Ganhador();
+
+    }
+
+    private string Jogador(Button btn)
+    {
+        if (vez == "X")
+        {
+            btn.Text = "X";
+            return vez = "O";
+        }
+        else
+        {
+            btn.Text = "O";
+            return vez = "X";
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+    }
+
+    private int? Jogadas()
+    {        
+        return jogada++;
+    }
+
+    private string VerificaVitoria()
+    {
+        
+        string vitoria = VerificaLinha();
+        if (!String.IsNullOrEmpty(vitoria))
         {
-            count++;
+            return vitoria;
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        
+        vitoria = VerificaColuna();
+        if (!String.IsNullOrEmpty(vitoria))
+        {
+            return vitoria;
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        
+        vitoria = VerificaDiagonais();
+        if (!String.IsNullOrEmpty(vitoria))
+        {
+            return vitoria;
+        }
+
+        
+        return VerificaEmpate();
+    }
+
+
+    private string VerificaEmpate()
+    {
+        for (int row = 0; row < rowBoard; row++)
+        {
+            for (int col = 0; col < colBoard; col++)
+            {
+                if (board[row, col] == null)
+                {
+                    return "";
+                }
+            }
+        }
+
+        if (maxSquare == jogada)
+        {
+            return "V";
+        }
+        else
+        {
+            return "";
         }
     }
+
+
+    public string VerificaLinha()
+    {
+        for (int row = 0; row < rowBoard; row++)
+        {
+            int contX = 0;
+            int contO = 0;
+
+            for (int col = 0; col < colBoard; col++)
+            {
+                if (board[row, col].Text == "X")
+                {
+                    contX++;
+                }
+                else if (board[row, col].Text == "O")
+                {
+                    contO++;
+                }
+            }
+
+            if (contX == colBoard)
+            {
+                return "X";
+            }
+            else if (contO == colBoard)
+            {
+                return "O";
+            }
+        }
+
+        return "";
+    }
+
+
+    private string VerificaColuna()
+    {
+        for (int col = 0; col < colBoard; col++)
+        {
+            int contX = 0;
+            int contO = 0;
+
+            for (int row = 0; row < rowBoard; row++)
+            {
+                if (board[row, col].Text == "X")
+                {
+                    contX++;
+                }
+                else if (board[row, col].Text == "O")
+                {
+                    contO++;
+                }
+            }
+
+            if (contX == rowBoard)
+            {
+                return "X";
+            }
+            else if (contO == rowBoard)
+            {
+                return "O";
+            }
+        }
+
+        return "";
+    }
+
+
+    private string VerificaDiagonais()
+    {
+        int contX = 0;
+        int contO = 0;
+
+        for (int i = 0; i < rowBoard; i++)
+        {
+            if (board[i, i].Text == "X")
+            {
+                contX++;
+            }
+            else if (board[i, i].Text == "O")
+            {
+                contO++;
+            }
+        }
+
+        if (contX == rowBoard)
+        {
+            return "X";
+        }
+        else if (contO == rowBoard)
+        {
+            return "O";
+        }
+
+        contX = 0;
+        contO = 0;
+
+        for (int i = 0; i < rowBoard; i++)
+        {
+            if (board[i, rowBoard - i - 1].Text == "X")
+            {
+                contX++;
+            }
+            else if (board[i, rowBoard - i - 1].Text == "O")
+            {
+                contO++;
+            }
+        }
+
+        if (contX == rowBoard)
+        {
+            return "X";
+        }
+        else if (contO == rowBoard)
+        {
+            return "O";
+        }
+
+        return "";
+    }
+
+
+    private void Zerar()
+    {
+
+        foreach (Button square in board)
+        {
+            square.Text = "";
+            square.IsEnabled = true;
+        }
+
+        vitoria = "início";
+    }
+
+
+    private void Ganhador()
+    {
+        String? ganhador = VerificaVitoria();
+        switch (ganhador)
+        {
+            case "X":
+                DisplayAlert("Parabéns!", $"O jogador {ganhador} ganhou!", "OK");
+                Zerar();
+                break;
+            case "O":
+                DisplayAlert("Parabéns!", $"O jogador {ganhador} ganhou!", "OK");
+                Zerar();
+                break;
+            case "V":
+                DisplayAlert("Parabéns!", $"O jogador {ganhador} ganhou!", "OK");
+                Zerar();
+                break;
+        }
+
+    }
+
 
 }
