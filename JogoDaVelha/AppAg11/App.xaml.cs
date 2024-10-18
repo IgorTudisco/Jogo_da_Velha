9ce15a82072809b10cc9086fa717a30f789fa23c
Igor Tudisco <igortudisco@hotmail.com>
2024-10-18 16:55:15 -0300

    Initial project

---
+++ JogoDaVelha/AppAg11/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppAg11
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
